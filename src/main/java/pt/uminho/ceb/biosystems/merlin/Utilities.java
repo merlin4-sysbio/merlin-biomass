e9806daa9e081f60a771ed5b32c59406f42b6bc0
Oscar Dias <odias@ceb.uminho.pt>
2019-02-08 17:39:59 +0000

    update compounds not readable by biojava

--- src/main/java/pt/uminho/ceb/biosystems/merlin/Utilities.java
+++ src/main/java/pt/uminho/ceb/biosystems/merlin/Utilities.java
@@ -126,7 +126,7 @@ public class Utilities {
 				products = products.concat((-entityComposition.get(monomer))+" ").concat(monomer.getName()).concat(" + ");
 		}
 		
-		System.out.println(macromolecule +" re " + reactants);
+		System.out.println(macromolecule +" re " + reactants.length());
 		System.out.println(entityComposition);
 		reactants = reactants.substring(0, reactants.length()-3);
 		products = products.substring(0, products.length()-3);
